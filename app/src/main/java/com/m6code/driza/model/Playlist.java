632ee81b66351ea8a373aa87adb0450ee805658c
M6code <benjamynfrank101@gmail.com>
2020-09-09 21:32:47 +0100

    Pojos

--- app/src/main/java/com/m6code/driza/model/Playlist.java
+++ app/src/main/java/com/m6code/driza/model/Playlist.java
@@ -1,4 +1,25 @@
 package com.m6code.driza.model;
 
+import java.util.ArrayList;
+
 public class Playlist {
+    private String id;
+    private String title;
+    private String duration;
+    private String nb_tracks;
+    private String link;
+    private String pictureMed;
+    private String trackListUrl;
+    private String creationDate;
+    private String creator; // creator.name
+    private String type;
+    private ArrayList<Track> trackList; // tracks.data
+
+    public Playlist() {
+    }
+
+    public String getDetails() {
+        return type + " (" + nb_tracks + ") tracks " + " duration " + duration
+                + "by " + creator + " created: " + creationDate;
+    }
 }
--- app/src/main/java/com/m6code/driza/model/Track.java
+++ app/src/main/java/com/m6code/driza/model/Track.java
@@ -1,4 +1,21 @@
 package com.m6code.driza.model;
 
 public class Track {
+    private String id;
+    private String title;
+    private String link;
+    private String duration;
+    private String trackPos;
+    private String releaseDate;
+    private String previewUrl;
+    private String artist; // artist.name
+    private String album; // album.title
+    private String type;
+
+    public Track() {
+    }
+
+    public String getDetails() {
+        return type + " Released: " + releaseDate;
+    }
 }
